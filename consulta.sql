8efb9cad2e15b4db9b4b2611a74196a97b8b406e
David <damirsa2001@gmail.com>
2024-10-25 20:02:29 +0200

    modificada consulta de prueba

--- consulta.sql
+++ consulta.sql
@@ -2,10 +2,11 @@ SELECT *
 FROM cypher(
     'el_grefo',
     $$
-    MATCH p = (nodo_inicio:Estacion)-[rel:RUTA*1..3]->(nodo_fin:Estacion) WHERE nodo_inicio.ciudad = 'Valencia' AND nodo_fin.ciudad = 'Madrid'
+    MATCH p = (nodo_inicio:Estacion)-[rel:RUTA*1..5]->(nodo_fin:Estacion)
+    WHERE nodo_inicio.id = 6 AND nodo_fin.id = 11
     UNWIND rel AS r
-    WITH nodo_inicio, nodo_fin, p, SUM(r.distancia) AS total_distancia
-    WHERE total_distancia <= 500
+    WITH nodo_inicio, nodo_fin, p, SUM(r.distancia) AS total_distancia, SUM(r.precio_billete) AS total_precio
+    WHERE total_precio <= 300
     RETURN p
     ORDER BY total_distancia ASC
     LIMIT 3
