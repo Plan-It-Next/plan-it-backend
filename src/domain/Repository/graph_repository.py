7b1425872722750040bba789a772786f64ec1f8d
David <damirsa2001@gmail.com>
2024-10-25 19:58:00 +0200

    resolve git conflicts

--- src/domain/Repository/graph_repository.py
+++ src/domain/Repository/graph_repository.py
@@ -111,6 +111,10 @@ class GraphRepository:
         consulta = """
             SELECT *
             FROM cypher(
+        async def maxSetFilterTrip():
+        '''
+        SELECT *
+            FROM cypher(
                 'el_grefo',
                 $$
                 MATCH p = (nodo_inicio:Estacion)-[rel:RUTA*1..3]->(nodo_fin:Estacion)
@@ -133,6 +137,8 @@ class GraphRepository:
 
         # 3. Agregamos el UNWIND y calculamos las sumas en WITH
         consulta += """
+                MATCH p = (nodo_inicio:Estacion)-[rel:RUTA*1..5]->(nodo_fin:Estacion)
+                WHERE nodo_inicio.id = 6 AND nodo_fin.id = 11
                 UNWIND rel AS r
                 WITH nodo_inicio, nodo_fin, p,
                      SUM(r.distancia) AS total_distancia,
@@ -155,6 +161,8 @@ class GraphRepository:
 
         # 5. Añadimos el RETURN, ORDER BY y LIMIT
         consulta += """
+                WITH nodo_inicio, nodo_fin, p, SUM(r.distancia) AS total_distancia, SUM(r.precio_billete) AS total_precio
+                WHERE total_precio <= 300
                 RETURN p
                 ORDER BY total_distancia ASC
                 LIMIT 3
